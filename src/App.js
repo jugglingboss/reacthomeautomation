4c3c172a5aa8881a7e522143d33a29e71e9e2409
Michael Babilya <michael.babilya@ansys.com>
2020-03-31 14:54:11 -0400

    clikc button to show message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App(props) {
+  const [toggle, setToggle] = useState(false);
+
+  const modifyToggle = () => {
+
+    setToggle(!toggle)
+  }
   return (
     <div className="App">
       <h1>{props.message}</h1>
       <button onClick={() => console.log("hi")}>Button</button>
-      <button onClick={() => <ShowMessage/>}>Button</button>
+      <button onClick={modifyToggle}>Button</button>
+      {toggle && <ShowMessage />}
+      <AcceptInput/>
     </div>
   );
 }
@@ -15,6 +23,18 @@ function App(props) {
 function ShowMessage () {
   return(
     <p>You found the top secret message</p>
+  )
+}
+
+function AcceptInput () {
+  return(
+    <form onSubmit={}>
+      <input
+        type="text"
+        placeholder="here"
+      />
+      <button>Submit</button>
+    </form>
   )
 }
 
